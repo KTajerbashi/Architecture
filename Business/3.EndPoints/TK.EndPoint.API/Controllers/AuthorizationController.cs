d1e87bdecd12534373fef109eb371a464cc7f089
Tajerbashi <kamrantajerbashi@gmail.com>
2024-08-24 21:29:46 +0330

    Feature : Refactor Codes

--- Business/3.EndPoints/TK.EndPoint.API/Controllers/AuthorizationController.cs
+++ Business/3.EndPoints/TK.EndPoint.API/Controllers/AuthorizationController.cs
@@ -1,15 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SOAP_EmployeeService;
 
 namespace TK.EndPoint.API.Controllers;
 
 public class AuthorizationController : BaseController
 {
-    [HttpGet("GetEmployeeInfo")]
-    public async Task<IActionResult> GetEmployeeInfo()
-    {
-        IEmployeeService employeeService = new EmployeeServiceClient(EmployeeServiceClient.EndpointConfiguration.BasicHttpBinding_IEmployeeService);
-        var result = employeeService.GetOrganizationContract();
-        return await Task.FromResult(Ok(result));
-    }
+  
 }
